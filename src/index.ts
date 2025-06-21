4bad2b4f23476e8c824908944998e3d3aaf0403d
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-06-22 00:40:15 +0300

    build index.ts and index.js

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, {Express} from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import http from 'http';
+
+const app: Express = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("PORT", 3000);
+app.set("BASE_URL", "http://localhost:3000");
+
+dotenv.config();
+
+try {
+    server.listen(app.get("PORT"), () => {
+        console.log(`Server running on port ${app.get("PORT")}`);
+    });
+} catch (error) {
+    console.log(error);
+}
+
+export default server;
