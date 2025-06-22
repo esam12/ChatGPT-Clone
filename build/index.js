e9784e6cc44b68b3c3293eac1341fdced982c634
ISAM ELZOBI <mobarmeg.9@gmail.com>
2025-06-22 18:40:56 +0300

    feat: add routes middleware to express app
    Added the routes middleware to handle API requests under '/api/v1' path.
    This enables the router system for the application.

--- build/index.js
+++ build/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
 const http_1 = __importDefault(require("http"));
+const routes_1 = __importDefault(require("./routes/routes"));
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
 app.use((0, cors_1.default)());
@@ -15,6 +16,7 @@ app.use(express_1.default.urlencoded({ extended: true }));
 app.set("PORT", 3000);
 app.set("BASE_URL", "http://localhost:3000");
 dotenv_1.default.config();
+app.use("/api/v1", routes_1.default);
 try {
     server.listen(app.get("PORT"), () => {
         console.log(`Server running on port ${app.get("PORT")}`);
